d589fa6e0d521105fa36d93ec9e1172b46ba6977
Lucas Xavier <lucas@umprogramador.com.br>
2020-08-16 15:13:59 +0000

    configurado ambiente de producao

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Limpando cache";
+
+php artisan cache:clear
+php artisan config:clear
+#php artisan route:clear
+
+echo "Executando configs";
+
+
+php artisan config:cache
+#php artisan route:cache
+php artisan optimize
+
+echo "Finalizado";
