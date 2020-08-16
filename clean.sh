4e0c69e796bccc0a24978abfe260afbe8b4f406a
Lucas Xavier <lucas@umprogramador.com.br>
2020-08-16 15:16:15 +0000

    Corrigido arquivo de limpar cache

--- clean.sh
+++ clean.sh
@@ -4,13 +4,13 @@ echo "Limpando cache";
 
 php artisan cache:clear
 php artisan config:clear
-#php artisan route:clear
+php artisan route:clear
 
 echo "Executando configs";
 
 
 php artisan config:cache
-#php artisan route:cache
+php artisan route:cache
 php artisan optimize
 
 echo "Finalizado";
