ffb0040e24a6b776680d08c81e66f0e1ee90dc6d
Lucas Xavier <ldfavier@gmail.com>
2020-09-04 11:43:55 -0300

    Descomentado notificacao

--- app/Http/Controllers/Api/PagseguroController.php
+++ app/Http/Controllers/Api/PagseguroController.php
@@ -270,8 +270,6 @@ class PagseguroController extends Controller
         $User = new User();
         $user = $User->where('uuid', $xml->reference)->first();
 
-        dd($user);
-
         if ($user) :
             $array = [
                 'titulo' => 'Status da compra',
